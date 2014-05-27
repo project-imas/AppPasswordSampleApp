ba4f3fc5185e7d762ebe100dcc97e001519f3f8f
Gregg Ganley <gganley@mitre.org>
2014-05-27 16:08:33 -0400

    more app-password udpates

---
+++ AppPassword.framework/Versions/A/Headers/APPassProtocol.h
@@ -0,0 +1,26 @@
+//
+//  APPassport.h
+//  AppPassword
+//
+//  Created by ct on 4/2/13.
+//  Copyright (c) 2013 Mitre. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol APPassProtocol <NSObject>
+
+-(void) APPassComplete:(UIViewController*) viewController
+            withPhrase:(NSString*)         phrase;
+
+// -----------------------------------------------------------------------------
+// Implement if using secure foundation
+// -----------------------------------------------------------------------------
+@optional
+
+-(BOOL) verifyPhraseWithSecureFoundation:(NSString*) phrase;
+
+-(void) resetPassAP;
+
+-(void) forgotPassAP;
+@end
